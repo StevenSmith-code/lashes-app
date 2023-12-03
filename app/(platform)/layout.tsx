2584880d3dc70e4f939e5d7583c1173625a1f9a0
Steven <digitalexile99@gmail.com>
2023-12-03 16:58:50 -0700

    feat: adding clerkjs auth

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Toaster } from 'sonner';
+
+import { ClerkProvider } from '@clerk/nextjs';
+
+export default function PlatformLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <Toaster />
+      {children}
+    </ClerkProvider>
+  );
+}
