737f2fe2d949229fa82df98acb6d561c0d732d68
Steven <digitalexile99@gmail.com>
2023-12-03 19:17:10 -0700

    style: change h1 typo

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -32,7 +32,7 @@ export default function MarketingPage() {
           New website launched
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6 max-w-4xl">
-          Experience the allure of flawless lashes and a radiant tan in
+          Experience the allure of flawless lashes or a radiant tan in
         </h1>
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-2 w-fit ">
           one place.
