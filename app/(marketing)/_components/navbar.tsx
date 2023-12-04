51a3e8bc0d070d5d16b559cd649f36e44e322098
Steven <digitalexile99@gmail.com>
2023-12-03 17:46:34 -0700

    feat: adding friendly ui when logged in

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,19 +1,35 @@
+"use client";
 import React from 'react';
 
 import Link from 'next/link';
 
 import Logo from '@/components/logo';
 import { Button } from '@/components/ui/button';
+import {
+  UserButton,
+  useUser,
+} from '@clerk/nextjs';
 
 export function Navbar() {
+  const { user } = useUser();
+  console.log(user);
   return (
     <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
-        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size={"sm"} variant={"outline"} asChild>
-            <Link href={"/sign-in"}>Login</Link>
-          </Button>
+        <div className="space-x-4 md:w-auto flex items-center justify-between w-full">
+          {!user ? (
+            <Button size={"sm"} variant={"outline"} asChild>
+              <Link href={"/sign-in"}>Login</Link>
+            </Button>
+          ) : (
+            <div className="flex items-center justify-center">
+              <p className="text-md mr-4 text-neutral-400 text-center">
+                Welcome back {user?.firstName}!
+              </p>
+              <UserButton />
+            </div>
+          )}
           <Button size={"sm"} asChild>
             <Link href={"/sign-up"}>Make an appointment</Link>
           </Button>
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 import { Toaster } from 'sonner';
 
-import { ClerkProvider } from '@clerk/nextjs';
-
 export default function PlatformLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <>
       <Toaster />
       {children}
-    </ClerkProvider>
+    </>
   );
 }
